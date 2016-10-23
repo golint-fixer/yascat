205e7b9e51ffbf08d8a6f1853c40d4afe95ca922
doriable <doria.keung@gmail.com>
2016-10-23 00:01:39 -0700

    Added tests to main and bot. Still working on tests for handlers and soundboard.

--- bot/bot_test.go
+++ bot/bot_test.go
@@ -1,8 +1,13 @@
 package bot
 
 import (
-	"json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestNewBot(t *testing.T) {
+	data := make([]byte, 0)
+	yascat := NewBot(data)
+	assert.Equal(t, bot{}, yascat, "NewBot expected to return bot struct.")
+}
